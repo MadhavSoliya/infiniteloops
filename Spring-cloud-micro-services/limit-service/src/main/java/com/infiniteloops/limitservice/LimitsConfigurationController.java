1625851547f2ef8621f859226e036bb4fc072772
MadhavSoliya <madhavsoliya1@gmail.com>
2019-10-21 19:37:37 +0530

    Update LimitsConfigurationController.java

--- Spring-cloud-micro-services/limit-service/src/main/java/com/infiniteloops/limitservice/LimitsConfigurationController.java
+++ Spring-cloud-micro-services/limit-service/src/main/java/com/infiniteloops/limitservice/LimitsConfigurationController.java
@@ -1,7 +1,6 @@
 package com.infiniteloops.limitservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
